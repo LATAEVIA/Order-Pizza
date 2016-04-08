1424d1d023c232103a6a500cd32518630936886a
LaTaevia <lataevia.berry@gmail.com>
2016-04-08 15:03:05 -0700

    add minimum requirments without readme file

--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,27 @@ this.pizzaCost = 0;
 }
 
 Pizza.prototype.pizzaCalc = function() {
-  if (this.pizzaSize === "personalPizza") {
+  if (this.pizzaSize === "personal") {
     this.pizzaCost =+ 5;
-  } else if (this.pizzaSize === "smallPizza") {
+  } else if (this.pizzaSize === "small") {
     this.pizzaCost =+ 7;
-  } else if (this.pizzaSize === "mediumPizza") {
+  } else if (this.pizzaSize === "medium") {
     this.pizzaCost =+ 9;
-  } else if (this.pizzaSize === "largePizza") {
+  } else if (this.pizzaSize === "large") {
     this.pizzaCost =+ 11;
   } else {
     alert("Pick your pie primeter please!");
   }
   for (var i = 0; i < this.pizzaToppings.length; i++) {
-    this.pizzaCost = this.pizzaCost + .5;
+    this.pizzaCost = this.pizzaCost + .50;
   }
 }
-
-// Pizza.prototype.orderAndPrice = function() {
-//   return this.newLocation + " " + this.newLandmark + " " + this.newSeason;
-// }
 
 // User Interface Logic
 $(document).ready(function(){
 
   $('form.pizza-order').submit(function(event){
-    event.preventDefault();
+
     $("input[type=checkbox]:checked").each(function(){
         pizzaToppings.push($(this).val());
         console.log(pizzaToppings);
@@ -42,6 +38,24 @@ $(document).ready(function(){
 
     newPizza.pizzaCalc();
     console.log(newPizza);
-    
+
+    $("#priceHide").show();
+    $("#priceOutput").text(newPizza.pizzaCost.toFixed(2));
+
+    // $("#sizeHide").show();
+    // $("#sizeOutput").text(newPizza.pizzaSize);
+    // if (newPizza.pizzaToppings.length >= 1) {
+    //   $("#toppingsHide").show();
+    //   $("#outputToppings li").each(function(index) {
+    //     $(this).append(newPizza.pizzaToppings[index]);
+    //     console.log(index);
+    //   });
+    // } else {
+    //   $("#toppingsOutput").append("<li>" + "Would you like to add toppings?" + "</li>");
+    // }
+
+    event.preventDefault();
   });
 });
+// $("#toppingsOutput").append("<li><span id='toppingsOutput'>" + newPizza.pizzaToppings + "</span></li>");
+// $("ul#outputToppings").append("<li><span id='toppingsOutput'>" + newPizza.pizzaCost + "</span></li>");
